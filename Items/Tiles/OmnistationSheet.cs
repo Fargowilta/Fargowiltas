03d87e6bf986e0db01133cf58dc7863125841ec6
Midnight295 <143055111+Midnight295@users.noreply.github.com>
2024-04-22 17:18:13 -0700

    omnistation resprite
    there are a few errors with it that i couldnt manage like theres some pink at the bottom and the glow doesnt animate

--- Items/Tiles/OmnistationSheet.cs
+++ Items/Tiles/OmnistationSheet.cs
@@ -25,6 +25,7 @@ namespace Fargowiltas.Items.Tiles
             LocalizedText name = CreateMapEntryName();
             // name.SetDefault("Omnistation");
             AddMapEntry(color, name);
+            AnimationFrameHeight = 72;
         }
 
         public override bool CanDrop(int i, int j) => false;
@@ -91,6 +92,24 @@ namespace Fargowiltas.Items.Tiles
             }
             int height = tile.TileFrameY == 36 ? 18 : 16;
             Main.spriteBatch.Draw(Request<Texture2D>(Texture + "_Glow").Value, new Vector2(i * 16 - (int)Main.screenPosition.X, j * 16 - (int)Main.screenPosition.Y) + zero, new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, height), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            AnimateTile;
         }
+       
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            frameCounter++;
+            if (frameCounter >= 5) //replace with duration of frame in ticks
+            {
+                frameCounter = 0;
+                frame++;
+                frame %= 42;
+            }
+        }
+
+
+
+
+
+
     }
 }
