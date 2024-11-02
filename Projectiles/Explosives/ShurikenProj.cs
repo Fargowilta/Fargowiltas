070390aa23fdea867ae36618cd55389121ac3826
JavyzTaken <jacklukas@outlook.com>
2024-11-02 18:42:13 +0100

    Reduced Boom Shuriken self damage

--- Projectiles/Explosives/ShurikenProj.cs
+++ Projectiles/Explosives/ShurikenProj.cs
@@ -42,7 +42,7 @@ namespace Fargowiltas.Projectiles.Explosives
 
         public override void ModifyHitPlayer(Player target, ref Player.HurtModifiers modifiers)
         {
-            modifiers.IncomingDamageMultiplier *= 4;
+            modifiers.IncomingDamageMultiplier *= 1.5f;
         }
 
         public override void OnKill(int timeLeft)
