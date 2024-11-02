609481523cc0fa912ca3cfbb611a92fb00889371
JavyzTaken <jacklukas@outlook.com>
2024-11-02 18:40:38 +0100

    Energized: Reduced Master+ damage by 1/1.2x, reduced HM boss damage per stack by 2

--- Fargowiltas.cs
+++ Fargowiltas.cs
@@ -53,10 +53,11 @@ namespace Fargowiltas
             {
                 float dmg;
                 if (HardmodeSwarmActive)
-                    dmg = 60 + 40 * SwarmItemsUsed;
+                    dmg = 60 + 38 * SwarmItemsUsed;
                 else
                     dmg = 50 + 3 * SwarmItemsUsed;
-
+                if (Main.masterMode)
+                    dmg /= 1.2f;
                 return (int)dmg;
             }
                 
