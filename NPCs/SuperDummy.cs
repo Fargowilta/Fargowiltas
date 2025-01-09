59a08c5f7f17b929b45cc914fa1d661c568c5359
JavyzTaken <jacklukas@outlook.com>
2025-01-09 21:41:15 +0100

    Super Dummy maximum health is now a 10th of the integer limit and isn't affected by difficulty scaling, and the npc is now unkillable

--- NPCs/SuperDummy.cs
+++ NPCs/SuperDummy.cs
@@ -20,11 +20,11 @@ namespace Fargowiltas.NPCs
 
 
         }
-
+        const int maxHP = int.MaxValue / 10;
         public override void SetDefaults()
         {
             NPC.CloneDefaults(NPCID.TargetDummy);
-            NPC.lifeMax = 50000;
+            NPC.lifeMax = maxHP;
             NPC.aiStyle = -1;
             NPC.width = 28;
             NPC.height = 50;
@@ -32,15 +32,18 @@ namespace Fargowiltas.NPCs
             NPC.npcSlots = 0;
             NPC.dontCountMe = true;
         }
-
+        public override void ApplyDifficultyAndPlayerScaling(int numPlayers, float balance, float bossAdjustment)
+        {
+            NPC.lifeMax = maxHP;
+        }
         public override bool? DrawHealthBar(byte hbPosition, ref float scale, ref Vector2 position) => false;
         public override void OnSpawn(IEntitySource source)
         {
-            NPC.life = NPC.lifeMax = 50000;
+            NPC.life = NPC.lifeMax = maxHP;
         }
         public override void AI()
         {
-            NPC.life = NPC.lifeMax = 50000;
+            NPC.life = NPC.lifeMax = maxHP;
 
             if (FargoGlobalNPC.AnyBossAlive())
             {
@@ -51,6 +54,10 @@ namespace Fargowiltas.NPCs
             }
         }
 
-        public override bool CheckDead() => false;
+        public override bool CheckDead()
+        {
+            NPC.life = NPC.lifeMax = maxHP;
+            return false;
+        }
     }
 }
